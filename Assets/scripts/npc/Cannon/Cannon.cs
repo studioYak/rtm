52ad29ae25ea3bfdc1c7df295b87ce690d010bba
Hugo <hugo.leray.scheidt@gmail.com>
2015-10-26 16:05:44 +0100

    Essai variable pour canon5

--- Assets/scripts/npc/Cannon/Cannon.cs
+++ Assets/scripts/npc/Cannon/Cannon.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic; // Lib for List<GameObject>
 
 /**
 * @author HugoLS
@@ -11,6 +12,7 @@ public class Cannon : NPC {
 
 	GameObject cannonBall;
 	GameObject projectile; 
+	private List<GameObject> projectiles;
 
 	bool fireInTheHall = false;
 	Vector3 target;
@@ -34,6 +36,7 @@ public class Cannon : NPC {
 	:base(2.0f, 0, Blocking.FREE, 60, 75, 0, "distance", "anonymous"){
 		// attackSpeed,  xpGain,  blocking,  hp,  damage,  movementSpeed,  attackType,  name
 		base.attackRange = 20.0f;
+		projectiles = new List<GameObject> ();
 	}
 
 	public override void Attack(Vector3 character)
@@ -45,16 +48,29 @@ public class Cannon : NPC {
 			base.Action.SetActionAsDistant();
 			fireInTheHall = true;
 			projectile = Instantiate(cannonBall) as GameObject;
+			projectiles.Add(projectile);
 			projectile.transform.position = new Vector3(transform.position.x,transform.position.y+3,transform.position.z);
 			Rigidbody rb = projectile.GetComponent<Rigidbody>();
 			float placementX = Mathf.Abs((character.x-transform.position.x)/(character.z-transform.position.z));
-			rb.velocity = transform.TransformDirection(-placementX,5,-15);
+			rb.velocity = transform.TransformDirection(-placementX,3,-25);
 			LastAttack = Time.time;
 		}
 		else
 		{
 			base.Action = new UnitAction(0,0,0);
 		}
+
+		if(projectiles.Count > 0)
+		{
+			int i = 0;
+			for(i=0;i<projectiles.Count;i++)
+			{
+				Rigidbody rb = projectiles[i].GetComponent<Rigidbody>();
+				float placementX = Mathf.Abs((character.x-projectiles[i].transform.position.x)/(character.z-projectiles[i].transform.position.z));
+				rb.velocity = projectiles[i].transform.TransformDirection(-placementX,3,-25);
+			}
+		}
+
 	}
 	
 }

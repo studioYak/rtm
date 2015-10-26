9b7bc84b4f569aec3568cfd45ede9949d71f0a50
Adrien Dourthe <adrien.dourthe@gmail.com>
2015-10-26 13:55:50 +0100

    hero GameModel

--- Assets/scripts/models/GameModel.cs
+++ Assets/scripts/models/GameModel.cs
@@ -1,7 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/**
+ * @author Adrien D
+ */
+
+/**
+ * This class is made to keep the context between controllers
+ */
 public class GameModel : MonoBehaviour {
 
+	/**
+	 * The hero : mainly to keep the class and XP
+	 */
+	private static Hero hero = new Warrior();
 
+	public static Hero Hero {
+		get {
+			return hero;
+		}
+
+		set {
+			hero = value;
+		}
+	}
 }
